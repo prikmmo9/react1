6806fbbf8570404ef7d152558e3eef6ca081b777
prikmmo9 <109761180+prikmmo9@users.noreply.github.com>
2023-02-07 01:11:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,8 +26,7 @@ function App() {
 
         <button onClick={onClose}>Закрыть2</button>
 
-        <iframe src="//6.annacdn.cc/8WaL3nf9VIyv/tv-series/13777" width="640" height="480" frameBorder="0"
-                allowFullScreen></iframe>
+       
 
 
     </div>
